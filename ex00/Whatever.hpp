1973f6f57d0a27ad873c84251f4ee76d6024f3b5
Piotr Wojnarowski <pwojnaro@3-B-12.42heilbronn.de>
2025-02-17 13:17:19 +0100

    adding templates for swap, min and max in Whatever.hpp

--- ex00/Whatever.hpp
+++ ex00/Whatever.hpp
@@ -6,8 +6,34 @@
 /*   By: pwojnaro <pwojnaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 12:54:25 by pwojnaro          #+#    #+#             */
-/*   Updated: 2025/02/17 12:55:49 by pwojnaro         ###   ########.fr       */
+/*   Updated: 2025/02/17 13:16:15 by pwojnaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
+template<typename T>
+
+void swap(T &a, T &b)
+{
+	if (&a != &b)
+	{
+		T temp(std::move(a));
+		a = std::move(b);
+		b = std::move(temp);
+	}
+}
+
+template<typename T>
+
+const T& min(const T &a, const T &b)
+{
+	return (a < b) ? a : b;
+}
+
+template<typename T>
+
+const T& max(const T &a, const T &b)
+{
+	return (a > b) ? a : b;
+}
