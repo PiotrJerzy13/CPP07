144def09c83d54262a82796215e3412ea78d7643
Piotr Wojnarowski <pwojnaro@2-A-2.42heilbronn.de>
2025-02-19 13:31:50 +0100

    reoving unused <cstddef>

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: pwojnaro <pwojnaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 13:28:07 by pwojnaro          #+#    #+#             */
-/*   Updated: 2025/02/17 13:34:49 by pwojnaro         ###   ########.fr       */
+/*   Updated: 2025/02/19 13:28:38 by pwojnaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 #include <iostream>
 #include <string>
-#include <cstddef>
 
 template <typename T, typename F>
 void iter(T* arr, size_t len, F func)
